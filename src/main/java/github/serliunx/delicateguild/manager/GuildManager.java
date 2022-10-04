94b024675d6892068fbeb15a879d9afc07bb800f
SerLiunx-ctrl <17689543@qq.com>
2022-10-04 20:44:52 +0800

    All guilds now automatically sorts and paginates based on rank

--- src/main/java/github/serliunx/delicateguild/manager/GuildManager.java
+++ src/main/java/github/serliunx/delicateguild/manager/GuildManager.java
@@ -6,21 +6,22 @@ import github.serliunx.delicateguild.entity.Guild;
 import github.serliunx.delicateguild.entity.guild.SimpleGuild;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
-
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GuildManager {
 
     private Map<String, Guild> guilds;
     private final DelicateGuild instance;
+    private List<Guild> guildTopList;
+    private final List<List<Guild>> guildPages;
 
     public GuildManager(){
         guilds = new HashMap<>();
         instance = DelicateGuild.getInstance();
-
+        guildPages = new ArrayList<>();
         loadAllGuilds();
+        guildReorder();
     }
 
     /**
@@ -84,7 +85,7 @@ public class GuildManager {
             instance.getLogger().warning("new guild data for " + guild.getId() + " failed updated to sql.");
             instance.getLogger().warning("you'd better go check your sql configuration.");
         }
-
+        guildReorder();
         return true;
     }
 
@@ -94,5 +95,66 @@ public class GuildManager {
      */
     public Map<String, Guild> getAllGuilds() {
         return guilds;
+    }
+
+    /**
+     * 根据公会等级重排序
+     */
+    private void guildReorder(){
+        guildTopList = new ArrayList<>(guilds.values());
+        guildTopList = guildTopList.stream().sorted(Comparator.comparing(Guild::getLevel).reversed())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 公会列表分页
+     * @param size 页面尺寸
+     */
+    private void guildPagination(int size){
+        guildPages.clear();
+        int page = 1, addition;
+        if(guildTopList.size() > size){
+            addition = (guildTopList.size() % size != 0) ? 1 : 0;
+            page = (guildTopList.size() / size) + addition;
+        }
+
+        int startIndex = 0, endIndex = size;
+        for(int i = 1; i <= page; i++){
+            if(page > 1){
+                if(i == page){
+                    guildPages.add(guildTopList.subList(startIndex, guildTopList.size()));
+                    break;
+                }
+                guildPages.add(guildTopList.subList(startIndex, endIndex));
+                startIndex += size;
+                endIndex += size;
+                continue;
+            }
+            guildPages.add(guildTopList);
+        }
+    }
+
+    /**
+     * 根据特定尺寸分割公会列表, 同时获取分页后的指定页码
+     * <li> 注意: 页码从 0 开始为第一页
+     * @param pageSize 页面尺寸 (如现有公会数量小于或等于页面尺寸会返回整个列表)
+     * @param page 需要返回的页码 (如页码不存在则会返回第一页)
+     * @return 指定页码的所有公会
+     */
+    public List<Guild> getReorderedPage(int pageSize, int page){
+        if(pageSize >= guildTopList.size()) return guildTopList;
+        guildPagination(pageSize);
+        if(!checkPageRange(page)) return guildPages.get(0);
+        return guildPages.get(page);
+    }
+
+    /**
+     * 检查指定页码的公会列表是否存在.
+     * <li> 注意: 页码从 0 开始为第一页
+     * @param page 页码
+     * @return 存在返回真, 否则返回假
+     */
+    public boolean checkPageRange(int page){
+        return page < (guildPages.size() + 1);
     }
 }
