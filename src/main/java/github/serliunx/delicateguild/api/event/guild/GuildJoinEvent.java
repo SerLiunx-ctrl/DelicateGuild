df483218ed722cab6e2e83a146707a2daaac011a
SerLiunx-ctrl <17689543@qq.com>
2022-10-11 20:46:00 +0800

    Added join_message support to GuildJoinEvent.

--- src/main/java/github/serliunx/delicateguild/api/event/guild/GuildJoinEvent.java
+++ src/main/java/github/serliunx/delicateguild/api/event/guild/GuildJoinEvent.java
@@ -1,7 +1,6 @@
 package github.serliunx.delicateguild.api.event.guild;
 
 import github.serliunx.delicateguild.entity.Guild;
-import github.serliunx.delicateguild.entity.Member;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
@@ -13,19 +12,31 @@ public class GuildJoinEvent extends GuildEvent implements Cancellable {
 
     private boolean cancelled = false;
 
-    private final Member memberAccept;
+    private final Player operator;
 
-    public GuildJoinEvent(@NotNull Player who, @NotNull Guild guild, @NotNull Member memberAccept) {
+    private String message;
+
+    public GuildJoinEvent(@NotNull Player who, @NotNull Guild guild, @NotNull Player operator,
+                          @NotNull String message) {
         super(who, guild);
-        this.memberAccept = memberAccept;
+        this.operator = operator;
+        this.message = message;
     }
 
     /**
      * 获取触发该事件时同意该申请的公会成员
      * @return 操作该申请的成员
      */
-    public Member getMemberAccept() {
-        return memberAccept;
+    public Player getOperator() {
+        return operator;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     @NotNull
